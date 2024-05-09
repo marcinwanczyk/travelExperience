ad698fbd6461633e28575fdc26d01155d59afd66
marcinwanczyk <marasek515@gmail.com>
2024-05-09 19:20:41 +0200

    add project to github

---
+++ src/main/java/com/example/world/TravelExperienceApplication.java
@@ -0,0 +1,13 @@
+package com.example.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelExperienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelExperienceApplication.class, args);
+	}
+
+}
