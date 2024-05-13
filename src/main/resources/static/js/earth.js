1353733d89c1cf9da761db5fb5d6488c2ab67b7b
marcinwanczyk <marasek515@gmail.com>
2024-05-13 21:29:43 +0200

    build: landing page positioning done, login/register form in process

--- src/main/resources/static/js/earth.js
+++ src/main/resources/static/js/earth.js
@@ -1,12 +1,13 @@
 let isMouseDown = false;
 let startX, startY;
-const canvasContainer = document.querySelector('#canvas-container')
+const canvasContainer = document.querySelector('.right')
 
 const scene = new THREE.Scene()
 const camera = new THREE.
     PerspectiveCamera(
         75,
         canvasContainer.offsetWidth / canvasContainer.offsetHeight,
+        // innerWidth / innerHeight,
         0.1,
         1000)
 const renderer = new THREE.WebGLRenderer(
@@ -19,8 +20,7 @@ const renderer = new THREE.WebGLRenderer(
 // renderer.setSize(innerWidth, innerHeight)
 renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight)
 renderer.setPixelRatio(window.devicePixelRatio)
-// document.getElementById('threejs').appendChild(renderer.domElement)
-document.body.appendChild(renderer.domElement)
+canvasContainer.appendChild(renderer.domElement)
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50),
     new THREE.MeshBasicMaterial
@@ -28,7 +28,7 @@ const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50),
 scene.add(sphere)
 
 const glowMap = new THREE.TextureLoader().load('/img/ring1.png')
-const glowMaterial = new THREE.MeshBasicMaterial({ map: glowMap, transparent: true, opacity: 0.2, blending: THREE.AdditiveBlending})
+const glowMaterial = new THREE.MeshBasicMaterial({ map: glowMap, transparent: true, opacity: 0.2, blending: THREE.AdditiveBlending })
 const glowMesh = new THREE.Mesh(new THREE.SphereGeometry(5.2, 50, 50), glowMaterial)
 // scene.add(glowMesh) 
 
@@ -54,7 +54,6 @@ starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVerti
 const stars = new THREE.Points(starGeometry, starMaterial)
 scene.add(stars)
 
-// scene.background = new THREE.Color(0x000000)
 camera.position.z = 20
 
 addEventListener('mousedown', (event) => {
@@ -80,11 +79,7 @@ addEventListener('mouseup', () => {
 
 function animate() {
     requestAnimationFrame(animate)
-    // sphere.rotation.x += 0.01;
     sphere.rotation.y += 0.001
-    // group.rotation.y += mouse.x 
-    // sphere.position.x = mouse.x * 10
-    // sphere.position.y = mouse.y * 10
     renderer.render(scene, camera)
 }
 animate()
