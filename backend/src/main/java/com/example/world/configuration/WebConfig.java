03bdf06c5bca6ac2d97af9bece313b47ab714e87
marcinwanczyk <marasek515@gmail.com>
2025-08-17 22:00:35 +0200

    fix: add import

--- backend/src/main/java/com/example/world/configuration/WebConfig.java
+++ backend/src/main/java/com/example/world/configuration/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
